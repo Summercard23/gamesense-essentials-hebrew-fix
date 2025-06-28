8e8053b22242e2a643caca2ec954ab4c3c3c5a1a
Summercard23 <59658908+Summercard23@users.noreply.github.com>
2025-06-28 16:48:44 +0300

    Update SongInformation.kt
    only change needed for Hebrew to work

--- src/main/kotlin/eu/tricht/gamesense/model/SongInformation.kt
+++ src/main/kotlin/eu/tricht/gamesense/model/SongInformation.kt
@@ -3,8 +3,19 @@ package eu.tricht.gamesense.model
 data class SongInformation(
     val fullSongName: String
 ) {
-    private val artistText = ScrollingText(fullSongName.split(" - ")[0])
-    private val songText = ScrollingText(fullSongName.split(" - ")[1])
+    private fun isHebrew(text: String): Boolean {
+        return text.any { it in '\u0590'..'\u05FF' }
+    }
+
+    private fun fixHebrew(text: String): String {
+        return if (isHebrew(text)) {
+            text.reversed()
+        } else text
+    }
+
+    private val artistText = ScrollingText(fixHebrew(fullSongName.split(" - ").getOrElse(0) { "" }))
+    private val songText = ScrollingText(fixHebrew(fullSongName.split(" - ").getOrElse(1) { "" }))
+
     fun artist() = artistText.text
     fun song() = songText.text
 }
